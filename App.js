433f267ef440be0a31df55f89c7d45494aff61ae
lixiao54 <lixiao54@meituan.com>
2023-02-01 15:51:00 +0800

    feat: 跑通

--- App.js
+++ App.js
@@ -1,25 +1,9 @@
-import logo from './logo.svg';
-import './App.css';
-import Counter from './example/demo1/src/Counter';
+import React from './miniReact/react';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-      <Counter></Counter>
+    <div>
+      我是mini react哟～
     </div>
   );
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import React from '../../../react';
-import ReactDOM from '../../../reactDom';
-import './index.css';
+import React from './miniReact/react';
+import ReactDOM from './miniReact/reactDom';
+
 import App from './App';
 
 ReactDOM.render(<App />, document.getElementById('root'));
