25ebe3a603cd655e32512c01606efd93dce4ec7a
lixiao54 <lixiao54@meituan.com>
2023-02-01 17:42:43 +0800

    feat: update

--- miniReact/domComponent.js
+++ miniReact/domComponent.js
@@ -1,15 +1,56 @@
+import { instantiate } from "./instantiate";
+
 export default class DomComponent {
   constructor(element) {
     this.tag = element.type;
+    this.props = element.props;
   }
 
   mount() {
     this.createElement();
+    this.mountChildren();
 
     return this.node;
   }
 
   createElement() {
     this.node = document.createElement(this.tag);
+  }
+
+  formatChildren(children) {
+    children = children || [];
+
+    if (!Array.isArray(children)) {
+      children = [children];
+    }
+
+    return children;
+  }
+
+  mountChildren() {
+    const children = this.formatChildren(this.props.children);
+
+    const nodeList = [];
+    const childComopnents = [];
+
+    children.forEach((childElement) => {
+      const component = instantiate(childElement);
+      if (component) {
+        childComopnents.push(component);
+      }
+
+      const node = component.mount();
+
+      if (node) {
+        nodeList.push(node);
+      }
+    });
+
+    this.childComponents = childComopnents;
+
+    // 挂载子节点
+    nodeList.forEach((node) => {
+      this.node.appendChild(node);
+    });
   }
 }
