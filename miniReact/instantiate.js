1fbca187345e0477c48b29702cf79cf679216660
lixiao54 <lixiao54@meituan.com>
2023-02-01 16:28:22 +0800

    feat: update

--- miniReact/instantiate.js
+++ miniReact/instantiate.js
@@ -1,9 +1,11 @@
 import DomComponent from "./domComponent";
 
 export function instantiate(element) {
+    console.log('🔥', element);
+
     if (typeof element.type === 'string') {
         return new DomComponent(element);
     }
-    
+
     return null;
 }
