aad761f36f895c7a3b3e2d0db6a8c73b0d6598ea
lixiao54 <lixiao54@meituan.com>
2023-02-01 16:15:52 +0800

    feat: update

--- miniReact/domComponent.js
+++ miniReact/domComponent.js
@@ -1,13 +1,7 @@
 export default class DomComponent {
-  constructor(element) {
-    this.element = element;
-    this.tag = element.type;
-    this.props = element.props;
-  }
 
   mount() {
     this.createElement();
-
     return this.node;
   }
 
